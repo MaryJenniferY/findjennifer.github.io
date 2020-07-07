9b256d3056b6feb844784a96f72b7e5c7db40432
MaryJenniferY <67652201+MaryJenniferY@users.noreply.github.com>
2020-07-07 08:52:17 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+ function picture1(){ 
+        var picc1 = "images/blog/pic1.png";
+     var picc2 = "images/blog/pic2.png";
+     var picc3 = "images/blog/pic3.png";
+     var picc4 = "images/blog/pic4.png";
+     var picc5 = "images/blog/pic5.png";
+     var picc6 = "images/blog/pic6.png";
+     var picc7 = "images/blog/pic7.png";
+        
+        
+    document.getElementById('pic1').src = picc1.replace('641x558', '20x20');
+
+    document.getElementById('pic2').src = picc2.replace('641x558', '20x20');
+
+    document.getElementById('pic3').src = picc3.replace('641x558', '20x20');
+
+    document.getElementById('pic4').src = picc4.replace('641x558', '20x20');
+     
+     document.getElementById('pic5').src = picc5.replace('641x558', '20x20');
+     
+     document.getElementById('pic6').src = picc6.replace('641x558', '20x20');
+     
+     document.getElementById('pic7').src = picc7.replace('641x558', '20x20');
+
+         
+     
+     document.getElementById('pic1').style.display='block';
+    document.getElementById('pic2').style.display='block';
+     document.getElementById('pic3').style.display='block';
+     document.getElementById('pic4').style.display='block';
+     document.getElementById('pic5').style.display='block';
+     document.getElementById('pic6').style.display='block';
+     document.getElementById('pic7').style.display='block';
+      
+     
+
+            }
+
+ 
